1cf5d0f163054d9b7c1c7d9c9d6ad7c8d47cec88
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-06-24 23:13:24 -0700

    Update remoteok_scraper.py
    Sending request

--- api_scraper/remoteok_scraper.py
+++ api_scraper/remoteok_scraper.py
@@ -1,3 +1,8 @@
+'''
+use source venv/bin/activate to activate virtual environment 
+use deactive to turn off virtual environment 
+'''
+import re
 import requests
 import xlwt
 from xlwt import Workbook
@@ -8,5 +13,23 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 
+# website being requested
+BASE_URL = 'https://remoteok.com/api/'
+
+# user agent lets website know to respond by determinig the device in use
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+
+REQUEST_HEADER = {
+    'User-Agent': USER_AGENT,
+    'Accept-Language': 'en-US, en;q=0.5',
+}
+
+
+def get_job_postings():
+    res = requests.get(url=BASE_URL, headers=REQUEST_HEADER)
+    return res.json()
+
+
 if __name__ == "__main__":
-    print("hello world")
+    json = get_job_postings()[1]
+    print(json)
