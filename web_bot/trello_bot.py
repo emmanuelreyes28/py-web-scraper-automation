accc0b9ae9d6baa4f70fcd01109bf13a484b774a
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-07-05 12:18:00 -0700

    Created trello_bot.py
    Imported all necessary libraries

---
+++ web_bot/trello_bot.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+from datetime import date
+import os
+import json
+
+#CHROME_DRIVE_PATH = os.path.join(os.getcwd(), "chromedriver")
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
